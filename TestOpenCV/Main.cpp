9bc0f4f4248e784dbe2bf77b50929b1b58f9c628
文昊 胡 <hwh19981124@hotmail.com>
2019-09-26 11:00:01 +0800

    edge_detector

--- TestOpenCV/Main.cpp
+++ TestOpenCV/Main.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 
+
 int main() {
-	Mat img = imread("test.JPG");
-	namedWindow("Picture");
-	imshow("Picture", img);
-	waitKey(6000);
+	Mat src, src_gray;
+	Mat grad;
+
+	const char* window_name = "Edge_Detector";
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
+	int c;
+
+	src = imread("test_2.png");
+	if (!src.data)
+	{
+		return -1;
+	}
+	GaussianBlur(src, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
+	cvtColor(src, src_gray, COLOR_RGB2GRAY);
+	namedWindow(window_name, WINDOW_AUTOSIZE);
+
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
+
+
+	Sobel(src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+
+	Sobel(src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT); 
+	convertScaleAbs(grad_y, abs_grad_y);
+
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+	imshow(window_name, grad);
+	waitKey();
 	return 0;
+
 }
